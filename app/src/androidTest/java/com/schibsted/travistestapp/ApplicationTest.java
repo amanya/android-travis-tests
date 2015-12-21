796b40008ecea7a4171a8c829bd1e209f9684e2d
Víctor González <victor.gonzalez.irimia@gmail.com>
2015-12-21 11:59:01 +0100

    Activate sample test.

--- app/src/androidTest/java/com/schibsted/travistestapp/ApplicationTest.java
+++ app/src/androidTest/java/com/schibsted/travistestapp/ApplicationTest.java
@@ -1,13 +1,35 @@
 package com.schibsted.travistestapp;
 
 import android.app.Application;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+
+@RunWith(AndroidJUnit4.class)
 public class ApplicationTest extends ApplicationTestCase<Application> {
   public ApplicationTest() {
     super(Application.class);
+  }
+
+  @Rule
+  public ActivityTestRule<MainActivity> activityRule =
+          new ActivityTestRule<>(MainActivity.class);
+
+  @Test
+  public void sampleTest() {
+    onView(withText("Hello World!")).check(matches(isDisplayed()));
   }
 }
